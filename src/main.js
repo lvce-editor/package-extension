927895680c07acd80fe04a56c73748417a80082a
Le Vivilet <levivlet@pm.me>
2024-03-23 13:03:14 +0100

    feature: add bundlejs helper function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
-export * from './parts/PackageExtension/PackageExtension.js'
+export * from './parts/BundleJs/BundleJs.js'
 export * from './parts/CopyFiles/CopyFiles.js'
+export * from './parts/PackageExtension/PackageExtension.js'
