4ee4a2cd1159e8cb0dc6666ab2ac447daa5030fd
Le Vivilet <levivilet@pm.me>
2025-12-13 18:04:19 +0100

    feature: update memory config

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,12 +1,6 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 515_000
+export const instantiations = 11_000
 
-export const instantiations = 14_000
-
-export const instantiationsPath = join(root, 'packages', 'test-worker')
-
-export const workerPath = join(root, '.tmp/dist/dist/testWorkerMain.js')
-
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const instantiationsPath = join(root, 'packages', 'package-extension')
--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,11 +1,12 @@
 import { measureMemory } from '@lvce-editor/measure-memory'
+<<<<<<< Updated upstream
 import { instantiations, instantiationsPath, playwrightPath, threshold, workerPath } from './config.ts'
+=======
+import { instantiations, instantiationsPath } from './config.ts'
+>>>>>>> Stashed changes
 
 const main = async () => {
   await measureMemory({
-    playwrightPath,
-    workerPath,
-    threshold,
     instantiations,
     instantiationsPath,
   })
