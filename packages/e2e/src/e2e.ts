bf35f6144d9a4cdbd677a36ec4ce299bb40c6e2b
Le Vivilet <levivilet@pm.me>
2025-12-13 19:31:55 +0100

    feature: add e2e package

---
+++ packages/e2e/src/e2e.ts
@@ -0,0 +1,8 @@
+const main = async () => {
+  // TODO run the package scripts with various fixtures
+  // verify the build works and
+  // helpful error messages are displayed when options are invalid
+  // or errors occur
+}
+
+main()
