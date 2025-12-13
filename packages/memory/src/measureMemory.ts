7b97ff3e2f40b5ea744709ae86057d8de2940391
Le Vivilet <levivilet@pm.me>
2025-12-13 18:04:57 +0100

    fix

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -1,19 +1,5 @@
 import { measureMemory } from '@lvce-editor/measure-memory'
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import { instantiations, instantiationsPath, playwrightPath, threshold, workerPath } from './config.ts'
-=======
 import { instantiations, instantiationsPath } from './config.ts'
->>>>>>> Stashed changes
-=======
-import {
-  instantiations,
-  instantiationsPath,
-  playwrightPath,
-  threshold,
-  workerPath,
-} from './config.ts'
->>>>>>> origin/main
 
 const main = async () => {
   await measureMemory({
