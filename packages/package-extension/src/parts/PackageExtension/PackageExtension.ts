15b5903522e9feb0e6d5a6bc752ccab398be11fe
Le Vivilet <levivilet@pm.me>
2025-12-13 21:27:44 +0100

    feature: enable setting version from git tag by default

--- packages/package-extension/src/parts/PackageExtension/PackageExtension.ts
+++ packages/package-extension/src/parts/PackageExtension/PackageExtension.ts
@@ -10,8 +10,8 @@ export const packageExtension = async ({
   highestCompression = false,
   inDir = process.cwd(),
   outFile = join(inDir, 'extension.tar.br'),
-  writeLastUpdatedFromGitCommit = false,
-  writeVersionFromGitTag = false,
+  writeLastUpdatedFromGitCommit = true,
+  writeVersionFromGitTag = true,
 }: Readonly<PackageExtensionOptions> = {}): Promise<void> => {
   const extensionJsonPath = join(inDir, 'extension.json')
   let extensionJson: Record<string, unknown> | null = null
