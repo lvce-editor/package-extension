b3f3be84dc9e8c2365baf7bf13faee28dc8d3ea5
Le Vivilet <levivilet@pm.me>
2025-12-13 19:21:20 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import config from '@lvce-editor/eslint-config'
+import * as config from '@lvce-editor/eslint-config'
 import actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
-  ...config,
+  ...config.default,
+  ...config.recommendedNode,
   ...actions,
+  // ...config
   {
     rules: {
       'github-actions/permissions': 'off',
