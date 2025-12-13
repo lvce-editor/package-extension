05b154c76ed9be97f47e62151e90a9e1995d9bd2
Le Vivilet <levivilet@pm.me>
2025-12-13 17:44:31 +0100

    feature: add eslint config (#50)

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import config from '@lvce-editor/eslint-config'
+import actions from '@lvce-editor/eslint-plugin-github-actions'
+
+export default [
+  ...config,
+  ...actions,
+  {
+    rules: {
+      'github-actions/permissions': 'off',
+    },
+  },
+]
